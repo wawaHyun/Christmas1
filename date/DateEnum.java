567242878fbe35b23009871f70fb80eb52cc6d62
wahyun <sjrkchdkdy@gmail.com>
2024-03-19 19:06:09 +0900

    Update DateEnum.java

--- date/DateEnum.java
+++ date/DateEnum.java
@@ -1,7 +1,33 @@
 package date;
 
 public enum DateEnum {
-//    public enum eee {
-//        양송이수프(5000);
-//    }
+양송이수프("양송이수프",6000),
+    타파스("타파스",5500),
+    시저샐러드("시저샐러드",8000),
+    ;
+    private final String name;
+    private final Integer pay;
+
+    Appetizer(String name, Integer pay) {
+        this.name = name;
+        this.pay = pay;
+    }
+
+    public String getAppName() {
+        return name;
+    }
+
+    public Integer getAppPay() {
+        return pay;
+    }
+public static String[] getAppNames(){
+        return Arrays.stream(Appetizer.values())
+                .map(Appetizer::getAppName)
+                .toArray(String[]::new);
+}
+public static Integer[]getAppPays(){
+        return Arrays.stream(Appetizer.values())
+                .map(i->i.getAppPay())
+                .toArray(Integer[]::new);
+}
 }
